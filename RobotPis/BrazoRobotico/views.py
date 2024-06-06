16f2c596b7bc879736340ccc38f8b6de251e6e37
josutrres <josutrres@gmail.com>
2024-06-06 10:51:55 -0400

    add BrazoRobotico views

--- RobotPis/BrazoRobotico/views.py
+++ RobotPis/BrazoRobotico/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def servo(request):
+    servos = Servo.objects.all()
+    return render(request, 'servo.html', {'servos': servos})
+
+def articulacion(request):
+    articulaciones = Articulacion.objects.all()
+    return render(request, 'articulacion.html', {'articulaciones': articulaciones})
+
+def base(request):
+    base = Base.objects.all()
+    return render(request, 'base.html', {'base': base})
+
--- RobotPis/RobotPis/urls.py
+++ RobotPis/RobotPis/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BrazoRobotico import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'), 
+    path('servo/', views.servo, name='servo'),
+    path('base/', views.base, name='base'),
 ]
