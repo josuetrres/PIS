be57a45922488fc9c3fde700ffb90d001a47c16e
vinirmn007 <alexis.roman@unl.edu.ec>
2024-06-07 01:27:29 -0500

    agregar template de testimonios, models y views

--- RobotPis/RobotPis/settings.py
+++ RobotPis/RobotPis/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'BrazoRobotico',
     'Carrito',
     'WebSite',
-    'Estadisticas',
+    'Testimonios',
 ]
 
 MIDDLEWARE = [
--- RobotPis/WebSite/views.py
+++ RobotPis/WebSite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from Carrito.models import *
+from Testimonios.views import *
 # Create your views here.
 
 def materiales(request):
     return render(request, 'materiales.html')
+
+def desarrolladores(request):
+    return render(request, 'desarrolladores.html')
